87b29cb40a16c82cac19c84b8bcd2f65e3702947
Vikash Sharma <kvikashsharma09@gmail.com>
2023-02-21 14:08:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./Component/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
