f88d37229ea09a0ccada53826f5551235535d358
Vikash Sharma <kvikashsharma09@gmail.com>
2023-02-22 18:02:36 +0530

    third commit(task-9)

--- src/Component/ExpenseItem.js
+++ src/Component/ExpenseItem.js
@@ -2,18 +2,17 @@
 
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-   const expenseDate = new Date(2021, 2, 28);
-   const expenseTitle = 'Car Insurance';
-   const expenseAmount = '267.13';
-   const LocationOfExpenditure = 'Automobile'
+function ExpenseItem(props) {
+   // const expenseDate = new Date(2021, 2, 28);
+   // const expenseTitle = 'Car Insurance';
+   // const expenseAmount = '267.13';
      return (
      <div className='expense-item'>
-        <div>{expenseDate.toISOString()}</div>
+        <div>{props.date.toISOString()}</div>
         <div className='expense-item__description'>
-         <h2>{expenseTitle}</h2>
-         <div className='Location-Expenditure'>{LocationOfExpenditure}</div>
-         <div className='expense-item__price'>${expenseAmount}</div>
+         <h2>{props.title}</h2>
+         <div className='location-Expenditure'>{props.location}</div>
+         <div className='expense-item__price'>${props.amount}</div>
         </div>
      </div>
      );
