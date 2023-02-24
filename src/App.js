cdb92447ba059afbf95f05756a93239da892bbbd
Vikash Sharma <kvikashsharma09@gmail.com>
2023-02-24 15:26:58 +0530

    sixth commit(task-12)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Expenses from "./Component/Expenses";
+import Expenses from "./Component/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
